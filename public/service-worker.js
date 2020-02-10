20c34a2917bc4ee300723b40a241e2f449bcbfd9
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-02-10 08:53:18 +0100

    Improve load times on slow connections

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,10 +14,25 @@ self.addEventListener('install', (event) => {
     );
 });
 
+/// Tries to cache a request, and times out after 500ms
+function try_cache(request) {
+    return new Promise(async accept => {
+        const timeout = setTimeout(accept, 500);
+        try {
+            const cache = await caches.open('v1');
+            await cache.add(request);
+        } catch (e) {
+            console.error("Failed to query " + request.url, e);
+        } finally {
+            clearTimeout(timeout);
+            accept();
+        }
+    });
+}
+
 self.addEventListener('fetch', (evt) => {
     evt.respondWith(new Promise(async (accept) => {
-        const cache = await caches.open('v1');
-        await cache.add(evt.request).catch(_ => _);
+        await try_cache(evt.request);
         const response = await caches.match(evt.request);
         accept(response || new Response("No network and no cache for " + evt.request.url, { status: 404 }));
     }));
