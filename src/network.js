2c1a8cdea8b66e164046ff53ffcb63af34188590
Noé <1livrebien@gmail.com>
2020-07-05 18:10:00 +0200

    fixed multiplayer

--- src/network.js
+++ src/network.js
@@ -1,4 +1,4 @@
-const TOKEN = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjA3NDk5YzJjYzVhZTg2NzQ3MmZmY2NmOTU0NmYwNDhkYzhhNTIwM2QzNmE3ZDNhYTg1ODBjNDIxOTlkODZiZmJiMWM2MGY0ZDJlN2Y4N2YwIn0.eyJhdWQiOiI4IiwianRpIjoiMDc0OTljMmNjNWFlODY3NDcyZmZjY2Y5NTQ2ZjA0OGRjOGE1MjAzZDM2YTdkM2FhODU4MGM0MjE5OWQ4NmJmYmIxYzYwZjRkMmU3Zjg3ZjAiLCJpYXQiOjE1Nzk4ODA3NjMsIm5iZiI6MTU3OTg4MDc2MywiZXhwIjoxNjExNTAzMTYzLCJzdWIiOiI0MDYiLCJzY29wZXMiOltdfQ.BhyfZqTDNLIZ0vKYVuox_H_UPIhnrOItSiUaYmE4x9CFar1g9F2yH8Pd421hVR0oQ8a9VARMionGg2R9kK8IAsbewLA_L2zS_96W_4MDU65_thTO3JcwNYd64TqW7WlrM7qQwK9YUVnBdniLhyQGelO2vlb3nOargaOsZ0mIxzB6C_wwUnzMTOcD_Pv16_mFfDm4g7I4uKlTCQqhNSgBIM_fa3lzuHlvP2_Np7aDWhM6c9ekk0PjiF991kghsfoF_HXFTkLsONsBZ7TIrHILfSq7hKolG-MO2eNezUOqkStIIGCF0vgu1flyUEJKSHJZ2cEyTb5VOdcN3riyK47Yj3ubZcC7khNZtLyNe0Cud9GLSo6WAXQiBYCkAqVSKO6ZGcAj8wNDp6efs30LKgiyc8jiMvyrgOLDDqcKdxcmyeqWDq9Ofut6yJEpKu2efl2UCxJUGsD-dXWNc06NSp3kuxzaB2ufppgrCZjCHet2e3eU_5qE5v2Nassx9NIGVpvlQO4EBstPt1QwI4vUIdeYGsjXTgMhRHYjPjHihKXRWaZznPl42Jfo8IO_YLU48cNGxu7fqwih8A-tXyXtsGT2w80I5BmLEs2IaosvqaofBsIYxj9t_R04BehMJKNMBMzuDdeDhN7fdkS0KEAezCQV4FPR7qPyZuwatsIf71wqYfs`
+const API_KEY = 'U4V7A1XSVvHeqoNsMuSGNc4Z7HBbWRR0PlhwYWk3fCzwCVPneOX2zy3TebAt'
 
 /**
  * Returns a random channel ID
@@ -16,7 +16,7 @@ export class Socket {
     constructor(channel_id, { onmove }) {
         this.id = random_peer_id();
         this.opponent = null;
-        this.websocket = new WebSocket(`wss://connect.websocket.in/v2/${channel_id}?token=${TOKEN}`);
+        this.websocket = new WebSocket(`wss://connect.websocket.in/v3/${channel_id}?apiKey=${API_KEY}`);
         this.websocket.onmessage = this.onmessage.bind(this);
         this.websocket.onopen = this.introduce.bind(this);
         this.onmove = onmove;
