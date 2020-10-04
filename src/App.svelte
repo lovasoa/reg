31d1f2bc91d082a1e9d1d141cd3fffed1045793a
ophir <pere.jobs@gmail.com>
2020-10-04 15:52:27 +0200

    Improve rules display

--- src/App.svelte
+++ src/App.svelte
@@ -45,6 +45,10 @@
     text-align: left;
   }
 
+  .ruleStyle li {
+    padding: 10px;
+  }
+
   button {
     border-radius: 50px;
     margin-bottom: 10px;
@@ -72,47 +76,47 @@
   {:else}
     <section class="validatable valid">
       {#if rules == true}
-        <div class="ruleStyle">
+        <ul class="ruleStyle">
           {#if language !== 'fr'}
-            <p>
-              * Each player in turn places a number between 1 and the total
-              number of squares of the grid (the maximal number is 16 for a 4x4
-              grid). Each number can only be placed once.
-            </p>
-            <p>
-              * On a same row, column, or diagonal, the numbers must be either
+            <li>
+              Each player in turn places a number between 1 and the total number
+              of squares of the grid (the maximal number is 16 for a 4x4 grid).
+              Each number can only be placed once.
+            </li>
+            <li>
+              On a given row, column, or diagonal, the numbers must be either
               increasing or decreasing
-            </p>
-            <p>
-              * A square becomes darker when there are no numbers that can go on
+            </li>
+            <li>
+              A square becomes darker when there are no numbers that can go on
               it. A player loses when they cannot play at their turn.
-            </p>
+            </li>
             <button on:click={_ => (language = 'fr')}>Français</button>
           {/if}
 
           {#if language == 'fr'}
-            <p>
-              * Chacun leur tour, les joueurs placent des nombres entre 1 et le
+            <li>
+              Chacun leur tour, les joueurs placent des nombres entre 1 et le
               nombre de cases total sur la grille (pour une grille en 4x4, le
               nombre maximal qu'on peut poser est 16). Chaque nombre ne peut
               être posé qu'une fois.
-            </p>
-            <p>
-              * Sur une même ligne, colonne, ou diagonale, les nombres doivent
+            </li>
+            <li>
+              Sur une même ligne, colonne, ou diagonale, les nombres doivent
               suivre un ordre croissant ou décroissant.
-            </p>
-            <p>
-              * Une case s'obscurcit lorsqu'on ne peut plus placer aucun nombre
+            </li>
+            <li>
+              Une case s'obscurcit lorsqu'on ne peut plus placer aucun nombre
               dessus. Un joueur perd lorsque c'est son tour et qu'il ne peut
               plus jouer.
-            </p>
+            </li>
             <button on:click={_ => (language = 'en')}>English</button>
           {/if}
 
           <button on:click={_ => ((opponent = null), (rules = false))}>
             Menu
           </button>
-        </div>
+        </ul>
       {:else}
         <button on:click={_ => (opponent = new AiOpponent())}>
           Play against an AI
